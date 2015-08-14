f306cf780e54a188acb3e7bdbc48cee8799ffd43
Kyle <kylerdanielster@gmail.com>
2015-08-14 15:15:10 -0500

    Added simple about page

---
+++ src/AppBundle/Controller/aboutController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class aboutController extends Controller
+{
+    /**
+     * @Route("/about", name="about")
+     */
+    public function aboutAction()
+    {
+      return $this->render('default/about.html.twig');
+
+      //   return new Response (
+      //     '<html><body> About Page </body></html>'
+      // );
+    }
+}
+
+// $html = $this->container->get('templating')->render(
+//     'example.html.twig',
+// );
+// return new Response($html);
